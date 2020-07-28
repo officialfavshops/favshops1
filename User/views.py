0dd0acafa1e4aacf8ebbf5f64d5d4dd2a78e835b
officialfavshops <officialfavshops@gmail.com>
2020-07-28 13:28:27 +0530

    carosule

--- User/views.py
+++ User/views.py
@@ -66,3 +66,9 @@ def forget_password(request):
     email = user.email
     return redirect('/')
     
+def forget_password_ajax(request):
+    if request.method == 'GET':
+        number = request.GET.get('number')
+        data = {
+            'exist_num' : User.objects.get(mobile_number = number).exist()
+        }
