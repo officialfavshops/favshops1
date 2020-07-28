d36fe3c48d56f04c1087686cdc6ff70bf53e2a5c
officialfavshops <officialfavshops@gmail.com>
2020-07-28 21:23:20 +0530

    forget_password

--- User/views.py
+++ User/views.py
@@ -62,7 +62,7 @@ def logout(request):
 def forget_password(request):
     mobile_number = request.POST['forget_mobile_number']
     message = ' '
-    user = User.objects.filter(mobile_number__iexact = mobile_number)
+    user = User.objects.get(mobile_number__iexact = mobile_number)
     email = user.email
     return redirect(request,'verify_otp.html',{'email':email})
 
